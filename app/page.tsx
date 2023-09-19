c2694f63270763c1973537e5dbfd8295be3797a1
Matthew Kwong <matthewkwong2@gmail.com>
2023-09-19 13:46:07 +0800

    feat: add bug minimal repo

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
+import { Box } from "@mui/material";
+import { NestedMui } from "../components/nested-mui";
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null
+  return (
+    <Box>
+      Hello world <NestedMui />
+    </Box>
+  );
 }
---
+++ components/nested-mui.tsx
@@ -0,0 +1,3 @@
+import { Button } from "@mui/material";
+
+export const NestedMui = () => <Button>test</Button>;
